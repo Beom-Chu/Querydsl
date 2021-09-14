419d5e478c60edd77a7235159313d0b2d960db67
kbs <chorocsky@gmail.com>
2021-09-14 13:40:45 +0900

    querydsl 환경 설정

--- src/test/java/com/kbs/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/kbs/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,7 @@ import javax.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import com.kbs.querydsl.entity.Hello;
 import com.kbs.querydsl.entity.QHello;
@@ -12,6 +13,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @SpringBootTest
 @Transactional
+//@Commit
 class QuerydslApplicationTests {
 
   @PersistenceContext
