b31550dffef633a170540d1c79f6324e89b90470
kbs <chorocsky@gmail.com>
2021-09-24 14:49:19 +0900

    정렬

--- src/test/java/com/kbs/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/kbs/querydsl/QuerydslBasicTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.swing.plaf.metal.MetalMenuBarUI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -156,7 +157,7 @@ class QuerydslBasicTest {
     assertThat(fetch.size()).isEqualTo(0);
   }
   
-  
+  @Disabled
   @Test
   public void searchResultTest() {
     
@@ -185,5 +186,33 @@ class QuerydslBasicTest {
     long fetchCount = queryFactory
       .selectFrom(member)
       .fetchCount();
+  }
+  
+  /**
+   * 회원 정렬 순서
+   * 1. 회원 나이 내림차순(desc)
+   * 2. 회원 이름 올림차순(asc)
+   * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+   */
+  @Test
+  public void sort() {
+    
+    em.persist(new Member(null, 100));
+    em.persist(new Member("member5", 100));
+    em.persist(new Member("member6", 100));
+    
+    List<Member> result = queryFactory
+      .selectFrom(member)
+      .where(member.age.eq(100))
+      .orderBy(member.age.desc(), member.username.asc().nullsLast())
+      .fetch();
+    
+    Member member5 = result.get(0);
+    Member member6 = result.get(1);
+    Member memberNull = result.get(2);
+    
+    assertThat(member5.getUsername()).isEqualTo("member5");
+    assertThat(member6.getUsername()).isEqualTo("member6");
+    assertThat(memberNull.getUsername()).isNull();
   }
 }
