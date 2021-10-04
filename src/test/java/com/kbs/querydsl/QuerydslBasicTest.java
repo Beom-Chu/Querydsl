5163d60f3c4ce0145a89540a05f0a1a09dbe9ff8
kbs <chorocsky@gmail.com>
2021-10-04 11:10:07 +0900

    결과반환

--- src/test/java/com/kbs/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/kbs/querydsl/QuerydslBasicTest.java
@@ -634,6 +634,7 @@ class QuerydslBasicTest {
   /**
    * 문자 더하기
    */
+  @Disabled
   @Test
   public void concat() {
     
@@ -648,5 +649,39 @@ class QuerydslBasicTest {
     }
   }
 
-  /** 모바일 테스트 */
+  
+  /**
+   * 프로젝션 대상이 하나
+   * 프로젝션 대상이 하나면 타입을 명확하게 지정할 수 있음
+   * 프로젝션 대상이 둘 이상이면 튜플이나 DTO로 조회
+   */
+  @Disabled
+  @Test
+  public void simpleProjection() { 
+    
+    List<String> result = queryFactory
+      .select(member.username)
+      .from(member)
+      .fetch();
+    
+    for (String s : result) {
+      System.out.println("username : "+ s);
+    }
+  }
+  
+  @Test
+  public void tupleProjection() {
+    
+    List<Tuple> result = queryFactory
+      .select(member.username, member.age)
+      .from(member)
+      .fetch();
+    
+    for (Tuple tuple : result) {
+      String username = tuple.get(member.username);
+      Integer age = tuple.get(member.age);
+      System.out.println(username);
+      System.out.println(age);
+    }
+  }
 }
