bf93fc064e2f8b972ac915b784c4184f94d13f44
kbs <chorocsky@gmail.com>
2021-10-11 18:28:59 +0900

    순수 JPA 리포지토리

---
+++ src/main/java/com/kbs/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,45 @@
+package com.kbs.querydsl.repository;
+
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import com.kbs.querydsl.entity.Member;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+
+/**
+ * 순수 JPA 리포지토리와 Querydsl
+ */
+@Repository
+public class MemberJpaRepository {
+
+  private final EntityManager em;
+  private final JPAQueryFactory jpaQueryFactory;
+
+  public MemberJpaRepository(EntityManager em) {
+    this.em = em;
+    this.jpaQueryFactory = new JPAQueryFactory(em);
+  }
+
+
+  public void save(Member member) {
+    em.persist(member);
+  }
+
+  public Optional<Member> findById(Long id) {
+    Member findMember = em.find(Member.class, id);
+    return Optional.ofNullable(findMember);
+  }
+
+  public List<Member> findAll() {
+    return em.createQuery("select m from Member m", Member.class)
+        .getResultList();
+  }
+  
+  public List<Member> findByUsername(String username) {
+    return em.createQuery("select m from Member m where m.username = :username", Member.class)
+          .setParameter("username", username)
+          .getResultList();
+  }
+}
